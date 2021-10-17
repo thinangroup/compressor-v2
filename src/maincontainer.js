b9e48261cd3855f4e32ba0f092fc8250b558a45d
jebulin <jebulin3@gmail.com>
2021-10-17 19:04:12 +0530

    changed contents
    Signed-off-by: jebulin <jebulin3@gmail.com>

--- src/maincontainer.js
+++ src/maincontainer.js
@@ -7,6 +7,8 @@ import MainContent from './maincontent';
 import Social from './components/social';
 import Rules from './rules';
 import RectangleGoogleAds from './GoogleAds/rectangle';
+import Content1 from './components/contents/content1';
+import Content2 from './components/contents/content2';
 
 export default function SimpleContainer() {
   return (
@@ -21,6 +23,8 @@ export default function SimpleContainer() {
       <Container maxWidth="md">
       <RectangleGoogleAds slot='7900942379'/>
       </Container>
+      <Content1/>
+      <Content2/>
       <Social position='relative'/>
     </React.Fragment>
   );
